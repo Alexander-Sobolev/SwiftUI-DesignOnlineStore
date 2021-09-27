44315a2b2ff0fd8b17e666aaf6945199ba9e9cb2
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-27 15:39:37 +0300

    Initial Commit

---
+++ SwiftUI-DesignOnlineStore/SwiftUI_DesignOnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignOnlineStoreApp.swift
+//  SwiftUI-DesignOnlineStore
+//
+//  Created by Alexander Sobolev on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignOnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
